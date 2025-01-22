0d11004d1dc2e9d7a3c1abb809ae71c513b943ec
HamzaJarane <70551889+HamzaJarane@users.noreply.github.com>
2025-01-22 14:50:25 +0100

    Add FooterProfileCard component and enhance blog page layout; update translations for author and related content

---
+++ src/components/Cards/FooterProfileCard.tsx
@@ -0,0 +1,53 @@
+import { getProfileData } from '@/helpers/getProfileData';
+import { Field, FullName, ProfileImage } from '@/helpers/StyledComponents';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+import tw from 'twin.macro';
+
+function FooterProfileCard() {
+    const data = getProfileData();
+    const { t } = useTranslation();
+    const profilePicture = `https://avatars.githubusercontent.com/u/${data.github.id}?v=${data.github.fetchedTime}`;
+
+    return (
+        <div
+            css={tw`flex gap-4`}
+        >
+            <ProfileImage
+                css={tw`rounded-full w-[110px]`}
+                src={profilePicture}
+                alt={"profilePicture"}
+                srcSet={profilePicture}
+            />
+
+            <div css={tw`flex flex-col justify-center`}>
+                <div>{t('blog.author')}</div>
+                <FullName>
+                    Hamza Jarane
+                </FullName>
+                <div>Web developer, DevOps</div>
+                <div css={tw`flex gap-2`}>
+                    <Field
+                        href={data.github.html_url}
+                        target={'_blank'}
+                    >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </Field>
+                    {data.general.map((social) => (
+                        <Field
+                            key={social.name}
+                            href={social.link}
+                            target={'_blank'}
+                        >
+                            <FontAwesomeIcon icon={social.icon} />
+                        </Field>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FooterProfileCard;
--- src/modules/Blog/Page.tsx
+++ src/modules/Blog/Page.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react'
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import tw from 'twin.macro';
 import Markdown from 'react-markdown'
-import { getPage } from '@/helpers/getBlog';
+import { getBlogPages, getPage } from '@/helpers/getBlog';
 import useSWR from 'swr'
 import { Loading } from '@/App';
 import rehypeRaw from 'rehype-raw'
 import './page.css';
+import { BlogCategory, BlogGrid, BlogRow } from '@/helpers/StyledComponents';
+import FooterProfileCard from '@/components/Cards/FooterProfileCard';
+import { useTranslation } from 'react-i18next';
 
 function Page() {
     const { slug } = useParams() as { slug: string };
     const { data, error, isLoading } = useSWR(slug, getPage);
+    const { t } = useTranslation();
+    const pages = getBlogPages();
+    const navigate = useNavigate();
+    const gotoPage = (slug: string) => navigate(`/blog/${slug}`);
 
     if (isLoading || !data) {
         return <Loading />;
@@ -21,14 +28,49 @@ function Page() {
     }
 
     return (
-        <div css={tw`flex justify-center items-center `}>
+        <div css={tw`flex flex-col justify-center items-center`}>
             <div css={tw`w-[90%] lg:w-[70%] py-6`}>
-                <Markdown 
+                <Markdown
                     rehypePlugins={[rehypeRaw]}
                     className={'markdown-body'}
                 >
                     {data}
                 </Markdown>
+            </div>
+
+            <div
+                css={tw`mt-10 w-[90%] lg:w-[70%]`}
+            >
+                <FooterProfileCard />
+            </div>
+
+            <div css={tw`text-center text-3xl font-semibold`}>
+                {t('blog.you-might-also-like')}
+            </div>
+
+            <div css={tw`w-[90%] lg:w-[70%] my-10 grid grid-cols-1 lg:grid-cols-3 gap-3`}>
+                {pages.sort(() => .5 - Math.random()).slice(0, 3).map((page) => (
+                    <BlogRow key={page.slug} onClick={() => gotoPage(page.slug)}>
+                        <div css={tw`font-semibold text-2xl text-center w-full`}>{page.title}</div>
+                        <div css={tw`text-sm`}>
+                            {page.description.length > 140 ? page.description.split('').splice(0, 140).join('') + '...' : page.description}
+                        </div>
+                        <div
+                            css={[
+                                tw`grid gap-2`,
+                                page.categories.length >= 3 ? tw`grid-cols-3` :
+                                    page.categories.length === 2 ? tw`grid-cols-2` :
+                                        page.categories.length === 1 && tw`grid-cols-1`
+                            ]}
+                        >
+                            {page.categories.map((category) => (
+                                <BlogCategory className='blogCategory'>
+                                    <div>{category}</div>
+                                </BlogCategory>
+                            ))}
+                        </div>
+                    </BlogRow>
+                ))}
             </div>
         </div>
     )
